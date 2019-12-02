8817ee62a24e47203da66f0e7d6db72fb94dcc5a
mesujitg <mesujitg@gmail.com>
2019-12-02 12:42:22 +0545

    added

--- app/src/main/java/com/example/class19sepapplication/ImInActivity.java
+++ app/src/main/java/com/example/class19sepapplication/ImInActivity.java
@@ -1,23 +1,35 @@
 package com.example.class19sepapplication;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
 
 public class ImInActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button buttonCamera,buttonWebsite,buttonGallery,buttonDialer;
     EditText editText;
     ImageView imageView;
+    Bitmap image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,13 +86,56 @@ public class ImInActivity extends AppCompatActivity implements View.OnClickListe
         super.onActivityResult(requestCode, resultCode, data);
 
         if(requestCode == 1){
-            Bitmap image = (Bitmap) data.getExtras().get("data");
+            image = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(image);
+            askPermission();
         }
 
         if(requestCode == 2){
             Uri uri = data.getData();
             imageView.setImageURI(uri);
+        }
+    }
+
+    public void askPermission(){
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+        }
+        else {
+            saveCapImg();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 1){
+            if(grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                saveCapImg();
+            }
+            else {
+                Toast.makeText(this, "No Permission", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void saveCapImg() {
+        //imageView.setImageBitmap(image);
+        try{
+            File mydir = new File(Environment.getExternalStoragePublicDirectory
+                    (Environment.DIRECTORY_PICTURES), "Myapp");
+            mydir.mkdir();
+            Random random = new Random();
+            int a = random.nextInt(1000);
+            File file = new File(mydir,"myimg"+a+".jpg");
+            FileOutputStream fos = new FileOutputStream(file);
+            image.compress(Bitmap.CompressFormat.PNG,100,fos);
+            Toast.makeText(this, "Saved to"+ mydir,
+                    Toast.LENGTH_SHORT).show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
         }
     }
 }
