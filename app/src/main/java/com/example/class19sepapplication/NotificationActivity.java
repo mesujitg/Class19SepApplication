4d4f08c41454647a0ef5686b7fd41d45ae01dd0f
mesujitg <mesujitg@gmail.com>
2020-01-21 21:25:56 +0545

    added

---
+++ app/src/main/java/com/example/class19sepapplication/NotificationActivity.java
@@ -0,0 +1,93 @@
+package com.example.class19sepapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.class19sepapplication.services.CreateChannel;
+import com.example.class19sepapplication.services.MyService;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    Button buttonN1,buttonN2,buttonS1,buttonS2;
+    NotificationManagerCompat notificationManagerCompat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+
+        buttonN1 = findViewById(R.id.btnNoti1);
+        buttonN2 = findViewById(R.id.btnNoti2);
+        buttonS1 = findViewById(R.id.btnServiceStart);
+        buttonS2 = findViewById(R.id.btnServiceStop);
+
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        CreateChannel channel = new CreateChannel(this);
+        channel.createChannel();
+
+        buttonN1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification1();
+            }
+        });
+
+        buttonN2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification2();
+            }
+        });
+
+        buttonS1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startMyService();
+            }
+        });
+
+        buttonS2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopMyService();
+            }
+        });
+    }
+
+    public void displayNotification1(){
+        Notification notification = new NotificationCompat
+                .Builder(this, CreateChannel.CHANNEL_1)
+                .setSmallIcon(R.drawable.my_icon_first)
+                .setContentTitle("First Notification")
+                .setContentText("This is first Notification")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManagerCompat.notify(1,notification);
+    }
+
+    public void displayNotification2(){
+        Notification notification = new NotificationCompat
+                .Builder(this, CreateChannel.CHANNEL_2)
+                .setSmallIcon(R.drawable.my_icon_first)
+                .setContentTitle("Second Notification")
+                .setContentText("This is Second Notification")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+        notificationManagerCompat.notify(2,notification);
+    }
+
+    public void startMyService(){
+        startService(new Intent(this, MyService.class));
+    }
+
+    public void stopMyService(){
+        stopService(new Intent(this, MyService.class));
+    }
+}
---
+++ app/src/main/java/com/example/class19sepapplication/services/MyService.java
@@ -0,0 +1,46 @@
+package com.example.class19sepapplication.services;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+
+    Context context = this;
+    Handler handler = null;
+    Runnable runnable = null;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Toast.makeText(context, "Service Started",
+                Toast.LENGTH_SHORT).show();
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, "Test",
+                        Toast.LENGTH_SHORT).show();
+                handler.postDelayed(runnable,2000);
+            }
+        };
+        handler.postDelayed(runnable,2000);
+    }
+
+    @Override
+    public void onDestroy() {
+        handler.removeCallbacks(runnable);
+        Toast.makeText(context, "Service Stopped",
+                Toast.LENGTH_SHORT).show();
+    }
+}
